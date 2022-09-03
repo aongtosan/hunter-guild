e64ab4197751aabfa7c55e9c2593a9e88fcf7319
aongtosan <aongtosan@gmail.com>
2022-09-03 09:14:59 +0700

    init script for tile engin generate

--- Assets/Resources/Scripts/TileGenerateEngine.cs
+++ Assets/Resources/Scripts/TileGenerateEngine.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class TileGenerateEngine : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    GameObject tilePrefab;
+    [SerializeField]
+    int width;
+    [SerializeField]
+    int height;
+    [SerializeField]
+    int depth;
+    [SerializeField]
+    int hillPercentage;
+    [SerializeField]
+    int missTilePercentage;
     void Start()
     {
         
