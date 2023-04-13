5fc6a5134e134e1e5e6ad975745a1e98b7947502
aongtosan <aongtosan@gmail.com>
2023-04-13 16:48:26 +0700

    update resource and dynamic list view unit list display

--- Assets/Resources/Scripts/InfoCards/InfoUnitListCard.cs
+++ Assets/Resources/Scripts/InfoCards/InfoUnitListCard.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoUnitListCard : MonoBehaviour
 {
@@ -9,6 +10,22 @@ public class InfoUnitListCard : MonoBehaviour
     public GameObject ui;
     public GameObject characterDataUi;
     // Update is called once per frame
+    public int unitCount;
+    void Start(){
+        for(int i=0;i<unitCount;i++){
+            GameObject unitCard = new GameObject("UnitCard");
+            unitCard.AddComponent<RectTransform>();
+            unitCard.AddComponent<CanvasRenderer>();
+            unitCard.AddComponent<Image>();
+            unitCard.gameObject.transform.parent = 
+            ui
+            .gameObject.transform.GetChild(0)
+            .gameObject.transform.GetChild(0)
+            .gameObject.transform.GetChild(0)
+            .gameObject.transform.GetChild(0);
+        }
+        
+    }
     void Update()
     {
           if(CombatManager.combatManager.Phase == CombatManager.CombatPhase.DEPLOY){
